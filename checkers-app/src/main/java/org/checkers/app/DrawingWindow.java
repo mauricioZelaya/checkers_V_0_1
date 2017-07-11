a4a2c000a8beef85169d4cddb12277edce91fcd8
Kenneth Perez <kenneth.perez@jalasoft.com>
2017-07-11 15:02:22 +0000

    CHK-011 Implement Player: Added comments

--- checkers-app/src/main/java/org/checkers/app/DrawingWindow.java
+++ checkers-app/src/main/java/org/checkers/app/DrawingWindow.java
@@ -1,4 +1,5 @@
 package org.checkers.app;
+
 import org.checkers.model.Player;
 import org.checkers.view.DrawingPanel;
 import org.checkers.view.PlayerView;
@@ -7,34 +8,35 @@ import org.checkers.controller.PlayerController;
 
 import java.awt.*;
 import javax.swing.JFrame;
+
 /**
- * Write a description of class DrawingWindow here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * Updated by Kenneth on 7/11/2017.
+ * Added the proper container to display the view of the player along with the view of the drawing
  */
 public class DrawingWindow extends JFrame {
-
+  // Creation of the Drawing Panel (The board)
   private DrawingPanel panel;
   private MenuBar menuBar;
+  // Creation of the controller
   private PlayerController miPlayerController;
 
+  //Drawing window construction
   public DrawingWindow(String title) {
     super(title);
     setLayout(new BorderLayout());
 
     panel = new DrawingPanel();
-    PlayerView newView = new PlayerView();
+
+    //Player Contruction
 
     Player miPlayerOne = new Player(1);
-    Player miPlayerTwo = new Player(1);
-
+    Player miPlayerTwo = new Player(2);
+    PlayerView newView = new PlayerView();
     PlayerController miPlayerController = new PlayerController(miPlayerOne, miPlayerTwo, newView);
-
+    // Menu bar construction
     menuBar = new MenuBar();
-
+    //Setting menubar and both panels to the main frame
     setJMenuBar(menuBar.getMenuBar());
-
     add(panel, BorderLayout.CENTER);
     add(newView, BorderLayout.EAST);
 
