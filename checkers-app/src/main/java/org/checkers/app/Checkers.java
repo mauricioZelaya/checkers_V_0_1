1847d83412442179d8bb3e5ef63cc6d670caf3a0
Kenneth Perez <kenneth.perez@jalasoft.com>
2017-07-11 13:16:21 +0000

    CHK-011 Implement Player: Fixed with size from 600 to 800 to have a correct display as soon as the GUI is initialized

--- checkers-app/src/main/java/org/checkers/app/Checkers.java
+++ checkers-app/src/main/java/org/checkers/app/Checkers.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class Checkers {
     public static void main(String args[]) {
         DrawingWindow window = new DrawingWindow("Checkers Game v0.1");
-        window.setSize(600, 600);
+        window.setSize(800, 600);
 
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
