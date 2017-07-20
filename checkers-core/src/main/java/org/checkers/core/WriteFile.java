2e0135af530fd1d11c9159a380611d02522a0122
carlos orellana <carlos.orellana@jalasoft.com>
2017-07-20 14:48:16 +0000

    Add the class necesary to create a file to save the final State and the historical movements

--- checkers-core/src/main/java/org/checkers/core/WriteFile.java
+++ checkers-core/src/main/java/org/checkers/core/WriteFile.java
@@ -1,5 +1,6 @@
 package org.checkers.core;
 
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,13 +10,18 @@ import java.io.PrintWriter;
  * The WriteFile class contains the method necessary to write and save document
  */
 public class WriteFile {
+
+  private final File file;
+  public WriteFile(File file){
+    this.file = file;
+  }
   /**
    * The method create a new document and write the info related row and column
    *
    * @param file  a file created to sabve the game
    * @param moves the movements performed in the game
    */
-  public void saveHistory(File file, String moves) {
+  public void saveHistory(String moves) {
     if (file.exists()) {
       writeFile(file, moves);
     }
@@ -27,7 +33,7 @@ public class WriteFile {
    * @param file  a file created to sabve the game
    * @param moves the movements performed in the game
    */
-  private void writeFile(File file, String infoToWrite) {
+  private void writeFile(File file, String moves) {
 
     FileWriter writer;
     PrintWriter line;
@@ -35,8 +41,7 @@ public class WriteFile {
     try {
       writer = new FileWriter(file, true);
       line = new PrintWriter(writer);
-
-      line.println(infoToWrite);
+      line.println(moves);
       line.close();
       writer.close();
     } catch (IOException e) {
@@ -53,16 +58,16 @@ public class WriteFile {
     FileWriter writer;
     PrintWriter line;
 
-    if (!file.exists()) {
-      try {
-        writer = new FileWriter(file, true);
-        line = new PrintWriter(writer);
+    try {
+      writer = new FileWriter(file, false);
+      line = new PrintWriter(writer, false);
         line.flush();
         line.close();
         writer.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
   }
 }
